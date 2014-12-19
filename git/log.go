f1a0377f7cc92404659032ba3b3f58e6121ccdd9
Yandong Yao <yyao@gopivotal.com>
2014-12-19 18:19:07 +0800

    add git log wrapper

---
+++ git/log.go
@@ -0,0 +1,16 @@
+package git
+
+import (
+	os/exec
+)
+
+func execGitLog(repoDir, remoteName, branchName string) ([]byte, error) {
+	if branchName == "" {
+		branchName = "HEAD"
+	}
+
+	var cmd *exec.Cmd = nil
+	if remoteName == "" {
+		cmd = exec.Command("git", "rev-list", `--since="2 months ago"`, `--header`, branchName)
+	} else {
+		cmd = exec.Command("git", "rev-list"
