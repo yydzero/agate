ca2cd101fa132c822f251c8db5dcbc8910c5b685
Yandong Yao <yyao@gopivotal.com>
2014-12-22 16:51:53 +0800

    add log.go

--- git/log.go
+++ git/log.go
@@ -1,16 +1,15 @@
 package git
 
-import (
-	os/exec
-)
+//import "os/exec"
 
-func execGitLog(repoDir, remoteName, branchName string) ([]byte, error) {
-	if branchName == "" {
-		branchName = "HEAD"
-	}
 
-	var cmd *exec.Cmd = nil
-	if remoteName == "" {
-		cmd = exec.Command("git", "rev-list", `--since="2 months ago"`, `--header`, branchName)
-	} else {
-		cmd = exec.Command("git", "rev-list"
+//func execGitLog(repoDir, remoteName, branchName string) ([]byte, error) {
+//	if branchName == "" {
+//		branchName = "HEAD"
+//	}
+//
+//	var cmd *exec.Cmd = nil
+//	if remoteName == "" {
+//		cmd = exec.Command("git", "rev-list", `--since="2 months ago"`, `--header`, branchName)
+//	} else {
+//		cmd = exec.Command("git", "rev-list"
